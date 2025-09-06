4a6a759924368ab0553a1024df6f696a586cfedc
suleman <sulemanhf06@gmail.com>
2025-09-06 18:19:09 +0500

    Graphing stuff

--- Graphing.py
+++ Graphing.py
@@ -239,36 +239,80 @@ plt.show()
 
 '''
 
-A histogram is a graph showing frequency distributions. It is a graph showing the number of observations within each given interval.
+A histogram is a graph showing frequency distributions and the number of observations within each given interval.
 In Matplotlib, we use the hist() function to create histograms. The hist() function will use an array of numbers to create a histogram, 
-the array is sent into the function as an argument. 
+the array is sent into the function as an argument.
 
-variable = numpy.random.uniform(Mean, x-intervals, y-intervals)
+'''
+
+# Uniform Data distribution using Histogram:
+
+'''
+
+The Python Program used to create an array where the values are concentrated around a given value will be:
+
+import numpy
+import matplotlib.pyplot as plt 
+
+variable = numpy.random.normal(S, x-intervals, y-intervals)
 hist(variable, n)
+plt.show()
 
-In probability theory this kind of data distribution is known as the normal data distribution, or the Gaussian data distribution, 
-after the mathematician Carl Friedrich Gauss who came up with the formula of this data distribution.
-
-Mean: This represents the mean of the normal distribution. It specifies the center of the distribution, where the peak of the bell 
-curve is located.
+S: This represents the point from where the value starts.
 
 x-intervals(width): This represents the standard deviation of the normal distribution. It determines the spread or "width" of the distribution,
 indicating how much the values deviate from the mean. A larger standard deviation results in a flatter, wider bell curve, while a 
 smaller standard deviation results in a taller, narrower one.
 
 y-intervals(height): This specifies the number of samples to draw from the distribution. In this case, 100,000 random numbers will 
-be generated, following a normal distribution with a mean of 5.0 and a standard deviation of 1.0
+be generated, following a uniform distribution with a starting point of 0.0 and a standard deviation of 5.0
 
-A normal distribution graph is also known as the bell curve because of it's characteristic shape of a bell.
+n = Number of Bars
 
-hist(variable, num of bars)
-
-Example:
+Example (Uniform data distribution):
 
 import numpy
 import matplotlib.pyplot as plt
 
-x = numpy.random.uniform(5.0, 1.0, 100000)
+x = numpy.random.uniform(0.0, 5.0, 100000)
+
+plt.hist(x, 100)
+plt.show()
+
+'''
+
+# Normal Data Distribution using Histogram:
+
+'''
+
+The Python Program used to create an array where the values are concentrated around a given value will be:
+
+import numpy
+import matplotlib.pyplot as plt 
+
+variable = numpy.random.normal(Peak value, x-intervals, y-intervals)
+hist(variable, n)
+plt.show()
+
+Peak value: It specifies the center of the distribution, where the peak of the bell curve is located.
+
+x-intervals(width): This represents the standard deviation of the normal distribution. It determines the spread or "width" of the distribution,
+indicating how much the values deviate from the mean. A larger standard deviation results in a flatter, wider bell curve, while a 
+smaller standard deviation results in a taller, narrower one.
+
+y-intervals(height): This specifies the number of samples to draw from the distribution. In this case, 100,000 random numbers will 
+be generated, following a normal distribution with a peak value of 5.0 and a standard deviation of 1.0.
+
+n = Number of Bars
+
+A normal distribution graph is also known as the bell curve because of it's characteristic shape of a bell.
+
+Example (Normal data distribution):
+
+import numpy
+import matplotlib.pyplot as plt
+
+x = numpy.random.normal(5.0, 1.0, 100000)
 
 plt.hist(x, 100)
 plt.show()
