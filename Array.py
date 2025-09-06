ac1ba3ff346ed366c05f384be9ab1177129888ac
suleman <sulemanhf06@gmail.com>
2025-09-06 16:33:15 +0500

    yes new stuff

---
+++ Array.py
@@ -0,0 +1,278 @@
+# Introduction:
+
+'''
+
+NumPy is used for working with array. It is short for "Numerical Python". It also has functions for working in domain of linear algebra, 
+fourier transform, and matrices. NumPy arrays are stored at one continuous place in memory unlike lists, so processes can access and 
+manipulate them very efficiently. This behavior is called locality of reference in computer science
+
+'''
+
+'''
+
+Example 1 (Creating Numpy array): 
+
+import numpy
+
+arr = numpy.array([1, 2, 3, 4, 5])
+print(arr)
+print(type(arr))
+
+'''
+
+'''
+
+Example 2 (Multidimensional arrays):
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4], ndmin=5)
+print(arr)
+print('number of dimensions :', arr.ndim)
+
+'''
+
+'''
+
+Example 3 (Checking Dimensions):
+
+import numpy as np
+
+a = np.array(42)
+b = np.array([1, 2, 3, 4, 5])
+c = np.array([[1, 2, 3], [4, 5, 6]])
+d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+say = print
+say(a)
+say(b)
+say(c)
+say(d)
+print(a.ndim)
+print(b.ndim)
+print(c.ndim)
+print(d.ndim)
+
+'''
+
+'''
+
+Example 4 (Creating an array containing 250 random floats between 0 and 5):
+
+import numpy
+
+x = numpy.random.uniform(0.0, 5.0, 250)
+print(x)
+
+'''
+
+# Array indexing:
+
+'''
+
+Array indexing is the same as accessing an array element. You can access an array element by referring to its index number.
+The indexes in NumPy arrays start with 0, meaning that the first element has index 0, and the second has index 1 etc.
+
+Example 1:
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4])
+
+print(arr[1])
+
+Example 2 (Adding two elements):
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4])
+
+print(arr[2] + arr[3])
+
+'''
+
+# Accessing 2D Arrays:
+
+'''
+
+To access elements from 2-D arrays we can use comma separated integers representing the dimension and the index of the element.
+Think of 2-D arrays like a table with rows and columns, where the dimension represents the row and the index represents the column.
+
+variable[row, column], always remember computing starts comuting from 0th index.
+
+Example:
+
+import numpy as np
+
+arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+
+print('5th element on 2nd row: ', arr[1, 4])
+
+'''
+
+# Accessing 3D Arrays:
+
+'''
+
+To access elements from 3-D arrays we can use comma separated integers representing the dimensions and the index of the element.
+
+Example of 3d array:
+d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+d[nth array, xth array in nth array, element of xth array in nth array]
+
+Example:
+
+import numpy as np
+
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+print(arr[0, 1, 2])
+
+'''
+
+# Negative Indexing:
+
+'''
+
+Its used to access an array from the end.
+
+Example:
+
+import numpy as np
+
+arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+
+print(''Last element from 2nd dim: '', arr[1, -1])
+
+'''
+
+# Slicing arrays:
+
+'''
+
+Slicing in python means taking elements from one given index to another given index.
+
+We slice an array like this: [start:end].
+We can also define the step, like this: [start:end:step].
+If we don't pass start its considered 0.
+If we don't pass end its considered length of array in that dimension.
+If we don't pass step its considered 1.
+
+Example 1:
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[1:5])
+
+Example 2:
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[1:5:2])
+
+'''
+
+# Negative Slicing:
+
+'''
+
+Use the minus operator to refer to an index from the end:
+
+Example:
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[-3:-1])
+
+'''
+
+# Slicing 2-D Arrays:
+
+'''
+
+Example:
+
+import numpy as np
+
+arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+print(arr[1, 1:4])
+
+'''
+
+# Data Type:
+
+'''
+
+Changing data type from float to integer by using int as parameter value.
+
+Example
+
+import numpy as np:
+
+arr = np.array([1.1, 2.1, 3.1])
+
+newarr = arr.astype(int)
+
+print(newarr)
+print(newarr.dtype)
+
+'''
+
+# Reshaping:
+
+'''
+
+Example 1:
+
+Convert the following 1-D array with 12 elements into a 2-D array.
+The outermost dimension will have 4 arrays, each with 3 elements:
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+newarr = arr.reshape(4, 3)
+print(newarr)
+
+Example 2:
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+newarr = arr.reshape(2, 3, 2)
+print(newarr)
+
+'''
+
+# Iterating Arrays:
+
+'''
+
+Example:
+
+import numpy as np
+
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+
+for x in arr:
+  print(x)
+
+'''
+
+# Sorting Arrays:
+
+'''
+
+Sorting means putting elements in an ordered sequence. Ordered sequence is any sequence that has an order corresponding to elements, 
+like numeric or alphabetical, ascending or descending. The NumPy ndarray object has a function called sort(), that will sort a specified array.
+We can sort any data type using sort() functon.
+
+Example:
+
+import numpy as np
+
+arr = np.array([3, 2, 0, 1])
+print(np.sort(arr))
+
+'''
---
+++ Graphing.py
@@ -0,0 +1,290 @@
+# Graphing:
+
+'''
+
+We can plot garphs in python using matplotlib libraray. Matplotlib is a low level graph plotting library in python
+that serves as a visualization utility. Most of the Matplotlib utilities lies under the pyplot submodule, 
+and are usually imported as plt in short. 
+
+The plot() function is used to draw points (markers) in a diagram. By default, the plot() function draws a line 
+from point to point. The function takes parameters for specifying points in the diagram. 
+Parameter 1 is an array containing the points on the x-axis. Parameter 2 is an array containing the points on the y-axis.
+
+Arguments in plot() function:
+
+1. marker = Gives a symbol to represent a marker (like *, x, o, D etc).
+2. mfc = Gives color to marker.
+3. ms = Gives size to marker.
+4. mec = Gives color to the edge of marker.
+5. linestyle = Gives a certain style to the line. example like ("--" , "..").
+6. color = Gives color to the line.
+7. linewidth =  Gives width to the line.
+
+1. The title() function gives a title to our graph.
+2. The xlabel() function gives a label to x-axis.
+3. The ylabel() function gives a label to y-axis.
+4. The show() function shows the graph in the new window.
+5. The grid() function is used to show the grid on the graph.
+6. The subplot() function makes multiple garphs in a single window. subplot(num of rows, num of columns, graph num).
+7. The colorbar() function gives a colorbar to the respected colors used to plot the values.
+
+
+The bar() function is used to make a bar graph. The argument of x-plane can be a string as most of the examples will have strings on x-plane.
+
+Arguments in bar() function:
+
+1. color = Gives color to the bar graph.
+2. width = Gives width to the bar graph.
+3. height = Gives height to the bar graph to barh() function. barh() is horizontal bar graph.
+
+'''
+
+'''
+
+Example 1(Drawing a line in graph from position (0,0) to position (6,250):
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints, marker = 'x')
+plt.show()
+
+'''
+
+'''
+
+Example 2(Ploting with different markers, marker size, markercolor):
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1, 2, 6, 8])
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(xpoints, ypoints , marker = 'o', ms = 20, mec = "green" , mfc = 'g', linestyle = "--")
+plt.show()
+
+'''
+
+'''
+
+Example 3(Ploting Multiple lines):
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x1 = np.array([0, 1, 2, 3])
+y1 = np.array([3, 8, 1, 10])
+x2 = np.array([0, 1, 2, 3])
+y2 = np.array([6, 2, 7, 11])
+
+plt.plot(x1, y1)
+plt.plot(x2, y2)
+plt.show()
+
+'''
+
+'''
+
+Example 4 (Enhancing the looks of garph):
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.title("Sports Watch Data")
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.plot(x, y)
+plt.grid()
+plt.show()
+
+'''
+
+'''
+
+Example 5 (Multiple graphs in one window):
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(2, 3, 1)
+plt.plot(x,y)
+
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(2, 3, 2)
+plt.plot(x,y)
+
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(2, 3, 3)
+plt.plot(x,y)
+
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(2, 3, 4)
+plt.plot(x,y)
+
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(2, 3, 5)
+plt.plot(x,y)
+
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(2, 3, 6)
+plt.plot(x,y)
+
+plt.show()
+
+'''
+
+# Scattered Graph:
+
+'''
+
+The scatter() function is used to plot scattered points in the graph.
+
+Arguments in scatter() function:
+
+1. color = The color argument can be used to give color to the scattered plotted values. 
+2. cmap = The cmap argument can be used within scatter function to give a scale of different colors to different scattered plotted values.
+3. alpha = It is used to adjust the transparency of the scattered plotted values.
+
+Example (Scattered Graph):
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+plt.scatter(x, y)
+
+x = np.array([2,2,8,1,15,8,12,9,7,3,11,4,7,14,12])
+y = np.array([100,105,84,105,90,99,90,95,94,100,79,112,91,80,85])
+plt.scatter(x, y)
+
+plt.show()
+
+Example (Color mapping of scattered garph):
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.random.randint(100, size=(100))
+y = np.random.randint(100, size=(100))
+colors = np.random.randint(100, size=(100))
+sizes = 10 * np.random.randint(100, size=(100))
+
+plt.scatter(x, y, c=colors, s=sizes, alpha=0.5, cmap='nipy_spectral')
+
+plt.colorbar()
+
+plt.show()
+
+'''
+
+# Bar Graph:
+
+'''
+
+The bar() function is used to make a bar graph. The argument of x-plane can be a string as most of the examples will have strings on x-plane.
+
+Arguments in bar() function:
+
+1. color = Gives color to the bar graph.
+2. width = Gives width to the bar graph.
+3. height = Gives height to the bar graph to barh() function. barh() is horizontal bar graph.
+
+Example (Bar Graph):
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.bar(x, y, color = "red", width = 0.5)
+plt.show()
+
+Example (Horizontal bar graph):
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.barh(x, y, color = "green", height = 1.5)
+plt.show()
+
+'''
+
+# Histogram:
+
+'''
+
+A histogram is a graph showing frequency distributions. It is a graph showing the number of observations within each given interval.
+In Matplotlib, we use the hist() function to create histograms. The hist() function will use an array of numbers to create a histogram, 
+the array is sent into the function as an argument. 
+
+numpy.random.uniform(starting point, x-intervals, y-intervals)
+hist(variable, num of bars)
+
+Example:
+
+import numpy
+import matplotlib.pyplot as plt
+
+x = numpy.random.uniform(0.0, 5.0, 100000)
+
+plt.hist(x, 100)
+plt.show()
+
+'''
+
+# Pie Charts:
+
+'''
+
+With Pyplot, we can use the pie() function to draw pie chart.
+
+Arguments in Pie Charts:
+
+1. labels = Can be a list of strings or integers to provide label the chart. The value of label is in correspondence with the index of array.
+2. startangle = Gives angle to the starting point of the pie chart.
+3. shadow = Gives shadow to the label.
+4. explode = Explodes out a part of pie chart to show a specific part of pie chart.
+5. colors = Gives color to the pie chart.
+
+Legend() function is used to add a list of explanation for each wedge. It has an argument called title which gives title to the legend.
+
+Example:
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = np.array([35, 25, 25, 15])
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+my = [0.2, 0 , 0,0]
+
+plt.pie(y, labels = mylabels, shadow=True, explode= my, startangle=90)
+plt.legend(title="Fruits")
+plt.show()
+
+'''
